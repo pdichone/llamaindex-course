1fd1dc309b36b9de8974522963d9de3d7ed0796f
Paulo <pdichone@gmail.com>
2024-10-14 12:06:54 -0700

    Update manual_rag.ts
    ResponseBuilder is no longer available!

--- manual_rag.ts
+++ manual_rag.ts
@@ -1,47 +1,81 @@
 /** @format */
 
 import fs from 'node:fs/promises';
+
 import dotenv from 'dotenv';
-import * as llamaIndex from 'llamaindex';
+
+/**
+ * Install the following dependencies:
+ *  npm install dotenv
+    npm install @types/node --save-dev
+  for openai api key
+ */
 
 // Load environment variables from .env file
 dotenv.config();
 
-async function manualRag() {
+// import {
+//   Document,
+//   MetadataMode,
+//   NodeWithScore,
+//   VectorStoreIndex,
+// } from 'llamaindex';
+
+import * as llamaIndex from 'llamaindex';
+
+
+
+
+async function manual_rag() {
+  // this is the same as the main function below,
+  // but we are manually creating the query engine
+  // and the retriever
+  // and the response builder
+  // and the synthesizer
+  // and the service context
+  // and the llm to show how it is done manually
+  // we are also customizing the prompt to show that you can do this easily with llamaIndex
   const documents = await new llamaIndex.SimpleDirectoryReader().loadData({
     directoryPath: './data',
   });
+
   const index = await llamaIndex.VectorStoreIndex.fromDocuments(documents);
 
   let customLLM = new llamaIndex.OpenAI();
-  let customEmbeddings = new llamaIndex.OpenAIEmbedding();
+  let customEmbedding = new llamaIndex.OpenAIEmbedding();
 
   let customServiceContext = llamaIndex.serviceContextFromDefaults({
     llm: customLLM,
-    embedModel: customEmbeddings,
+    embedModel: customEmbedding,
   });
 
   let customQaPrompt = function ({ context = '', query = '' }) {
     return `Context information is below.
-            ---------------------
-            ${context}
-            ---------------------
-            Given the context information, answer the query.
-            Include a random fact about lions and elephants in Africa in your answer.\
-            The lions and elephants can come from your training data.
-            Query: ${query}
-            Answer:`;
+        ---------------------
+        ${context}
+        ---------------------
+        Given the context information, answer the query.
+        Include a random fact about lions and elephants in Africa in your answer.\
+        The whale lions and elephants can come from your training data.
+        Query: ${query}
+        Answer:`;
   };
 
-  let customResponseBuilder = new llamaIndex.SimpleResponseBuilder(
-    customServiceContext,
-    customQaPrompt
-  );
+  // const res = await responseSynthesizer.synthesize({
+  //   query: "What age am I?",
+  //   nodesWithScore,
+  // });
+  // console.log(res);
 
-  let customSynthesizer = new llamaIndex.ResponseSynthesizer({
-    responseBuilder: customResponseBuilder,
-    serviceContext: customServiceContext,
-  });
+  // let customResponseBuilder = new llamaIndex.buil(
+  //   customServiceContext,
+  //   customQaPrompt
+  // );
+
+  // let customSynthesizer = getResponseSynthesizer({
+  //   responseBuilder: customResponseBuilder,
+  //   serviceContext: customServiceContext,
+  // });
 
   let customRetriever = new llamaIndex.VectorIndexRetriever({
     index,
@@ -49,14 +83,36 @@ async function manualRag() {
 
   let customQueryEngine = new llamaIndex.RetrieverQueryEngine(
     customRetriever,
-    customSynthesizer
+    // customSynthesizer
   );
 
   let response = await customQueryEngine.query({
     query: 'what is the article about?',
   });
-  console.log('\n\n === Custom Query Engine Response === \n\n');
 
   console.log(response.toString());
 }
-manualRag().catch(console.error);
+async function main() {
+  const documents = await new llamaIndex.SimpleDirectoryReader().loadData({
+    directoryPath: './data',
+  });
+
+  const index = await llamaIndex.VectorStoreIndex.fromDocuments(documents);
+
+  // create a query engine
+  // retriever, postprocessing,syntehsizer
+  const queryEngine = index.asQueryEngine();
+
+  // query the index
+  const resp = await queryEngine.query({
+    query: 'what is this article about?',
+  });
+  console.log(resp);
+}
+
+manual_rag().catch(console.error);
+// let customLLM = new llamaIndex.OpenAI();
+// let customEmbedding = new llamaIndex.OpenAIEmbedding();
+
+// put llm and embedding model into a ServiceContext object
+
